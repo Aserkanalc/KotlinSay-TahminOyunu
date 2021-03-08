540f7095fdfe7f572e94fa869b99e058206e6872
Serkan Alc <70329389+Aserkanalc@users.noreply.github.com>
2021-03-08 04:43:13 +0300

    Basic Sayı tahmin oyunu

---
+++ app/src/main/java/com/example/udemydeneme/SayıTahminOyunu.kt
@@ -0,0 +1,40 @@
+package com.example.udemydeneme
+
+import java.util.*
+import kotlin.random.Random
+
+fun main(){
+
+    var girdi=Scanner(System.`in`)
+    var hak= 100
+    var rastgeleSayı= Random.nextInt(0,101)
+
+    while(true){
+        if (hak==0){
+            print("Deneme Hakkınız Bitti :(")
+            break
+        }
+        else {
+            print("tahmininizi giriniz:")
+            var sayı = girdi.nextInt()
+            if (sayı < rastgeleSayı) {
+                println("daha büyük bir sayı dene")
+                hak -= 1
+                println("Kalan hakkınız $hak")
+                continue
+            }
+            if(sayı>rastgeleSayı){
+                println("daha küçük bir sayı dene")
+                hak -= 1
+                println("Kalan hakkınız $hak")
+                continue
+            }
+            else{
+                println("tebrikler doğru sayıyı buldunuz")
+                break
+            }
+        }
+
+    }
+
+}
